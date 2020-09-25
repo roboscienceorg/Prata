f4cf69aa3488c63d0e7890938fa9dfbbf4ac8a2e
Steidl <Ethan.Steidl@Mines.sdsmt.edu>
2020-09-25 16:51:56 -0600

    added classes to handle serialization for data transmission

--- Rust/src/channel.rs
+++ Rust/src/channel.rs
@@ -4,7 +4,8 @@
 //use std::collections::Ve
 use splay::SplayMap;
 use splay::SplaySet;
-
+use std::thread;
+use std::time::Duration;
 
 #[derive(Debug)]
 
@@ -56,13 +57,12 @@ impl Default for Ports
 
 pub struct Channel
 {
-     pub mode: ChannelMode,
-     pub name: String,
-     //publisher pub sender: ZMQ::Publisher()
-     //subscriber pub reciever: ZMQ::Subscriber()
-     pub info: self::data::Information,
+     pub mode: ChannelMode,             //mode of channel
+     pub name: String,                  //name of channel
+     pub port: u16,                     //port for listening
+     pub info: self::data::Information, //information storage
      //maps an ip to its port range
-     pub addressBook : SplayMap<i32,Ports>,
+     pub addressBook : SplayMap<i32,Ports>,  //blacklist stuff
      
 
 
@@ -75,6 +75,7 @@ impl Default for Channel
         {
             mode: ChannelMode::STANDARD,
             name: String::from("1"),
+            port: 55555,
             info: self::data::Information::new(),
             addressBook: SplayMap::new()
         }
@@ -108,6 +109,64 @@ impl Channel
           let mut ports = Ports { fullRange: false, ..Default::default() };
           self.addressBook.insert(ip, ports );
      
+     }
+
+     pub fn decode(&mut self, message: String)
+     {
+          /* stack overflow code convert string to binary
+
+          let name = "Jake".to_string();
+          let mut name_in_binary = "".to_string();
+      
+          // Call into_bytes() which returns a Vec<u8>, and iterate accordingly
+          // I only called clone() because this for loop takes ownership
+          for character in name.clone().into_bytes() {
+              name_in_binary += &format!("0{:b} ", character);
+          }
+          println!("\"{}\" in binary is {}", name, name_in_binary);
+          */
+          let messageType = message.chars().next().unwrap();
+
+          if (messageType == 'b')
+          {
+               println!("\"{}\" at index {} in binary is {:?}", message, 0, messageType);
+          }
+               /*
+          let mut index = 0;
+          let mut name_in_binary = "".to_string();
+          for character in message.clone().into_bytes() {
+               name_in_binary = format!("0{:b} ", character);
+               println!("\"{}\" at index {} in binary is {}", message, index, name_in_binary);
+               index = index + 1;
+          }
+          */
+     }
+     pub fn main(&mut self)
+     {
+          println!("sup");
+          let context = zmq::Context::new();
+          let responder = context.socket(zmq::REP).unwrap();
+
+          let mut str1 = String::from("tcp://*:");
+          let str_with_port = self.port.to_string();
+          let address = [str1, str_with_port].concat();
+          println!("value of s is {:?}", address);
+          let temp = String::from("abcdef");
+          self.decode(temp);
+          return;
+          
+          assert!(responder.bind(&address).is_ok());
+      
+          let mut msg = zmq::Message::new();
+          loop {
+              responder.recv(&mut msg, 0).unwrap();
+              println!("Received {}", msg.as_str().unwrap());
+              thread::sleep(Duration::from_millis(1000));
+              responder.send("World", 0).unwrap();
+
+
+
+          }
      }
 
 
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -5,6 +5,9 @@ mod channel;
 fn main() {
     let mut c = channel::Channel { mode: channel::ChannelMode::BLACKLIST, /*name: String::from("c1"),*/ ..Default::default()};
     c.add(44);
-    println!("Hello, world!");
+    println!("Heello, world!");
     println!("{}", c.to_string());
+    println!("Hello, world!");
+    c.main();
+    
 }

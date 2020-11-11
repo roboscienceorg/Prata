8418158ee4b77f71a5cca15f687d03499b858c43
Timothy Adcock <Timothy.Adcock@mines.sdsmt.edu>
2020-11-10 17:31:16 -0700

    added timing information

---
+++ TALA/Tests/test_time_julia.jl
@@ -0,0 +1,223 @@
+using Pkg
+Pkg.activate("..\\Build\\Julia\\TALA.jl")
+using TALA
+using Statistics
+
+REPEAT = 1000
+SLEEP = 2
+SPAM = 15
+
+function printStats(stats,title)
+    stats = [round(x, digits=5) for x in stats]
+    open("timestatsJulia.txt","a") do f
+        write(f, string(title, "\n"))
+        write(f,"##############\n")
+        write(f, string("Min: ", stats[1], "\n"))
+        write(f, string("Median: ", stats[2], "\n"))
+        write(f, string("Max: ", stats[3], "\n"))
+        write(f, string("Average: ", stats[4], "\n"))
+        write(f,"##############\n\n")
+    end
+
+end
+
+function getStats(times)
+    return [minimum(times), median(times), maximum(times), mean(times)]*1000
+end
+
+
+
+open("timestatsJulia.txt", "w") do f
+      write(f, "")
+end
+
+println("Startup of Network")
+times = []
+for i in 1:REPEAT
+     tmp = @elapsed begin
+        m = TALA.connect("127.0.0.1", 25565)
+        TALA.host(m)
+        TALA.terminate(m)
+    end
+    push!(times, tmp)
+end
+
+stats = getStats(times)
+printStats(stats,"Startup of network")
+sleep(SLEEP)
+
+
+println("Channel creation")
+times = []
+m = TALA.connect("127.0.0.1", 25565)
+TALA.host(m)
+s = TALA.subscriber(m)
+for i in 1:REPEAT
+     tmp = @elapsed begin
+        TALA.listen(s,string(rand(Int)))
+    end
+    push!(times, tmp)
+end
+stats = getStats(times)
+printStats(stats,"Channel creation")
+sleep(SLEEP)
+
+
+println("Listen function call time without known channel")
+times = []
+TALA.createChannel(m,25566, "NK", "BROADCAST", 1);
+for i in 1:REPEAT
+    local s = TALA.subscriber(m)
+     tmp = @elapsed begin
+        TALA.listen(s,"NK")
+    end
+    push!(times, tmp)
+end
+stats = getStats(times)
+printStats(stats,"Listen function call time without known channel")
+sleep(SLEEP)
+
+
+println("Listen function call time with known channel")
+times = []
+s = TALA.subscriber(m)
+TALA.createChannel(m,25567, "K", "BROADCAST", 1);
+TALA.subscriberConnect(s,"K")
+for i in 1:REPEAT
+     tmp = @elapsed begin
+        TALA.listen(s,"K")
+    end
+    push!(times, tmp)
+end
+stats = getStats(times)
+printStats(stats,"Listen function call time with known channel")
+sleep(SLEEP)
+
+
+println("Publish function call time without known channel")
+times = []
+for i in 1:REPEAT
+    local p = TALA.publisher(m)
+     tmp = @elapsed begin
+        TALA.publish(p,"NK","yes")
+    end
+    push!(times, tmp)
+end
+stats = getStats(times)
+printStats(stats,"Publish function call time without known channel")
+sleep(SLEEP)
+
+
+println("Publish function call time with known channel")
+times = []
+p = TALA.publisher(m)
+TALA.publisherConnect(p,"K")
+for i in 1:REPEAT
+     tmp = @elapsed begin
+        TALA.publish(p,"K","yes")
+    end
+    push!(times, tmp)
+end
+stats = getStats(times)
+printStats(stats,"Publish function call time with known channel")
+sleep(SLEEP)
+
+
+println("Full run without known channel")
+times = []
+m = TALA.connect("127.0.0.1", 25568)
+TALA.host(m)
+TALA.subscriberConnect(TALA.subscriber(m),"NK")
+for i in 1:REPEAT
+    local p = TALA.publisher(m)
+    local s = TALA.subscriber(m)
+    tmp = @elapsed begin
+        TALA.publish(p,"NK","yes")
+        TALA.listen(s,"NK")
+    end
+    push!(times, tmp)
+end
+stats = getStats(times)
+printStats(stats, "Full run without known channel")
+sleep(SLEEP)
+
+
+
+println("Full run with known channel")
+times = []
+m = TALA.connect("127.0.0.1", 25569)
+TALA.host(m)
+p = TALA.publisher(m)
+s = TALA.subscriber(m)
+TALA.subscriberConnect(s,"K")
+TALA.publisherConnect(p,"K")
+for i in 1:REPEAT
+     tmp = @elapsed begin
+        TALA.publish(p,"K","yes")
+        TALA.listen(s,"K")
+    end
+    push!(times, tmp)
+end
+stats = getStats(times)
+printStats(stats, "Full run with known channel")
+sleep(SLEEP)
+
+
+
+println("Spam Publish")
+times = []
+m = TALA.connect("127.0.0.1", 25570)
+TALA.host(m)
+p = TALA.publisher(m)
+TALA.publisherConnect(p,"K")
+for j in 1:REPEAT
+    tmp = @elapsed Threads.@threads for i in 1:SPAM
+        local p = TALA.publisher(m)
+            TALA.publish(p,"K","yes")
+    end
+        push!(times, tmp)
+end
+
+stats = getStats(times)
+printStats(stats,"Spam Publish")
+sleep(SLEEP)
+
+
+println("Spam Listen")
+times = []
+m = TALA.connect("127.0.0.1", 25570)
+s = TALA.subscriber(m)
+TALA.subscriberConnect(s,"K")
+for j in 1:REPEAT
+    tmp = @elapsed Threads.@threads for i in 1:SPAM
+        local s = TALA.subscriber(m)
+            TALA.listen(s,"K")
+    end
+        push!(times, tmp)
+end
+
+stats = getStats(times)
+printStats(stats,"Spam Listen")
+sleep(SLEEP)
+
+
+println("Spam Channel")
+cnt = 25570
+times = []
+
+
+for j in 1:REPEAT
+    m = TALA.connect("127.0.0.1", cnt)
+    TALA.host(m)
+    s = TALA.subscriber(m)
+    tmp = @elapsed Threads.@threads for i in 1:SPAM
+        TALA.subscriberConnect(s,string(rand(Int)))
+    end
+    TALA.terminate(m)
+    global cnt += 1
+    push!(times, tmp)
+end
+
+stats = getStats(times)
+printStats(stats,"Spam Channel")
+sleep(SLEEP)
---
+++ TALA/Tests/test_time_python.py
@@ -0,0 +1,233 @@
+import timeit
+import time
+import numpy as np
+import TALA
+
+REPEATS = 1000
+NUMBERS = 1 # will break if >1
+TIMEBETWEEN = 2
+SPAM = 15
+DOSPAM = True
+
+def printStats(stats,title):
+    f = open("timestatspython.txt","a")
+    stats = [round(x*1000,5) for x in stats]
+    f.write(title + '\n')
+    f.write("###################\n")
+    f.write("Min: " + str(stats[0]) + '\n')
+    f.write("Median: " + str(stats[1]) + '\n')
+    f.write("Max: " + str(stats[2]) + '\n')
+    f.write("Average: " + str(stats[3]) + '\n')
+    f.write("###################\n\n")
+    f.close()
+
+def getStats(times):
+    #print(*([round(x*1000,5) for x in times]))
+    return [np.min(times), np.median(times), np.max(times), np.average(times)]
+
+def closeDefault():
+    m = TALA.connect("127.0.0.1", 25565)
+    m.terminate()
+
+f = open("timestatspython.txt","w")
+f.write("")
+f.close()
+
+networkStartupSetup='''
+import TALA
+'''
+
+networkStartup='''
+m = TALA.connect("127.0.0.1", 25565)
+m.host()
+m.terminate()
+'''
+
+setupListenNK='''
+import TALA
+m = TALA.connect("127.0.0.1", 25566)
+m.createChannel(25567, "NK", "BROADCAST", 1);
+m.publisher().publish("NK","yes")
+s = m.subscriber()
+'''
+
+setupListenK='''
+import TALA
+m = TALA.connect("127.0.0.1", 25566)
+m.createChannel(25580, "K", "BROADCAST", 1);
+m.publisher().publish("K","yes")
+s = m.subscriber()
+s.connect("K")
+'''
+
+setupPublishNK='''
+import TALA
+m = TALA.connect("127.0.0.1", 25566)
+p = m.publisher()
+'''
+
+setupPublishK='''
+import TALA
+m = TALA.connect("127.0.0.1", 25566)
+p = m.publisher()
+p.connect("K")
+'''
+
+createChannel='''
+s.listen(str(randint(1,10000)))
+'''
+
+print("Startup of network")
+
+printStats(getStats(timeit.Timer(stmt=networkStartup,setup=networkStartupSetup).repeat(number=NUMBERS,repeat=(REPEATS))),"Startup of network")
+time.sleep(TIMEBETWEEN)
+
+print("Channel creation")
+m = TALA.connect("127.0.0.1", 25565)
+m.host()
+printStats(getStats(timeit.Timer(stmt=createChannel,setup="import TALA; from random import randint; m = TALA.connect('127.0.0.1', 25565);s=m.subscriber()").repeat(number=NUMBERS,repeat=(REPEATS))),"Channel creation")
+m.terminate()
+time.sleep(TIMEBETWEEN)
+
+print("Listen function call time without known channel")
+m = TALA.connect("127.0.0.1", 25566)
+m.host()
+printStats(getStats( timeit.Timer(stmt='s.listen("NK")', setup=setupListenNK).repeat(number=NUMBERS,repeat=REPEATS)),"Listen function call time without known channel")
+time.sleep(TIMEBETWEEN)
+
+print("Listen function call time with known channel")
+printStats(getStats(timeit.Timer(stmt='s.listen("K")', setup=setupListenK).repeat(number=NUMBERS,repeat=REPEATS)),"Listen function call time with known channel")
+time.sleep(TIMEBETWEEN)
+
+
+print("Publish function call time without known channel")
+printStats(getStats(timeit.Timer(stmt='p.publish("NK","yes")', setup=setupPublishNK).repeat(number=NUMBERS,repeat=REPEATS)),"Publish function call time without known channel")
+time.sleep(TIMEBETWEEN)
+
+print("Publish function call time with known channel")
+printStats(getStats(timeit.Timer(stmt='p.publish("K","yes")', setup=setupPublishK).repeat(number=NUMBERS,repeat=REPEATS)),"Publish function call time with known channel")
+TALA.connect("127.0.0.1", 25566).terminate()
+time.sleep(TIMEBETWEEN)
+
+fullNK='''
+import TALA
+m = TALA.connect("127.0.0.1", 25568)
+p = m.publisher()
+s = m.subscriber()
+'''
+
+print("Full run without known channel")
+m = TALA.connect("127.0.0.1", 25568)
+m.host()
+m.publisher().publish("NK","yes")
+time.sleep(TIMEBETWEEN)
+printStats(getStats(timeit.Timer(stmt='p.publish("NK","yes"); s.listen("NK")', setup=fullNK).repeat(number=NUMBERS,repeat=REPEATS)),"Full run without known channel")
+m.terminate()
+time.sleep(TIMEBETWEEN)
+
+
+fullK='''
+import TALA
+m = TALA.connect("127.0.0.1", 25568)
+p = m.publisher()
+s = m.subscriber()
+p.connect("K")
+s.connect("K")
+'''
+
+print("Full run with known channel")
+m = TALA.connect("127.0.0.1", 25568)
+m.host()
+time.sleep(TIMEBETWEEN)
+printStats(getStats(timeit.Timer(stmt='p.publish("K","yes"); s.listen("K")', setup=fullK).repeat(number=NUMBERS,repeat=REPEATS)),"Full run with known channel")
+m.terminate()
+time.sleep(TIMEBETWEEN)
+
+
+import threading
+if DOSPAM:
+
+    def sendPub(m):
+        m.publisher().publish("test","yes")
+
+    def spamPub():
+        threads = []
+        m = TALA.connect("127.0.0.1",25570)
+        for index in range(SPAM):
+            x = threading.Thread(target=sendPub, args=(m,))
+            threads.append(x)
+            x.start()
+
+        for thread in threads:
+            thread.join()
+
+
+
+    print("Spam Publish")
+    m = TALA.connect("127.0.0.1",25570)
+    m.host()
+    m.publisher().publish("test","yes")
+    time.sleep(TIMEBETWEEN)
+    printStats(getStats(timeit.Timer(stmt=spamPub).repeat(number=NUMBERS,repeat=(REPEATS))), "Spam Publish")
+    m.terminate()
+    time.sleep(TIMEBETWEEN)
+
+
+    def sendLis(m):
+        m.subscriber().listen("test")
+
+    def spamLis():
+        threads = []
+        m = TALA.connect("127.0.0.1",25572)
+
+        for index in range(SPAM):
+            x = threading.Thread(target=sendLis, args=(m,))
+            threads.append(x)
+            x.start()
+
+        for thread in threads:
+            thread.join()
+
+
+    print("Spam Listen")
+    m = TALA.connect("127.0.0.1",25572)
+    m.host()
+    m.publisher().publish("test","yes")
+    time.sleep(TIMEBETWEEN)
+    printStats(getStats(timeit.Timer(stmt=spamLis).repeat(number=NUMBERS,repeat=(REPEATS))), "Spam Listen")
+    m.terminate()
+    time.sleep(TIMEBETWEEN)
+
+    from random import randint
+
+    def sendChan(m,chan):
+
+        p = m.publisher()
+        s = m.subscriber()
+        p.connect(chan)
+        s.connect(chan)
+        p.publish(chan,"t")
+        s.listen(chan)
+
+
+    def spamChan():
+        threads = []
+        m = TALA.connect("127.0.0.1",25574)
+        tmp = str(randint(1,10000))
+        m.publisher().connect(tmp)
+        for index in range(SPAM):
+            x = threading.Thread(target=sendChan, args=(m, tmp,))
+            threads.append(x)
+            x.start()
+
+        for thread in threads:
+            thread.join()
+
+
+
+    print("Spam Channel Creation")
+    m = TALA.connect("127.0.0.1",25574)
+    m.host()
+    time.sleep(TIMEBETWEEN)
+    printStats(getStats(timeit.Timer(stmt=spamChan).repeat(number=NUMBERS,repeat=(REPEATS))), "Spam Channel Creation")
+    m.terminate()

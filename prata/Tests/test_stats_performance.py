6215ec7a919457aeb0350248cdf179fa0279ad9f
ES3580 <esteidl@live.com>
2020-11-29 15:10:07 -0700

    Changed python test cases to be pass fail in performace

--- prata/Tests/test_stats_performance.py
+++ prata/Tests/test_stats_performance.py
@@ -2,22 +2,23 @@ import prata as tl
 import time
 import json
 
+print("TEST: checking connection speed to Master Process")
 m = tl.connect("127.0.0.1", 25565)
 
 m.host()
-print("setting up host...")
+#setting up host
 time.sleep(1)
 
 sub = m.subscriber()
 pub = m.publisher()
 
-print("sending messages to 100 channels")
+#sending messages to 100 channels
 i = 0
 while i < 100:
     pub.publish(str(i),str(i))
     i += 1
 
-print("getting status from master process with 100 channels 100 times")
+#getting status from master process with 100 channels 100 times
 start = time.process_time()
 i = 0
 while i < 100:
@@ -27,7 +28,9 @@ end = time.process_time()
 
 elapsed_time = (end - start) / 100
 
-print("pinging status for 100 channels took", elapsed_time, "seconds on average")
+if(elapsed_time > 0.5)
+    print("---TEST: failure pinging status for 100 channels took", elapsed_time, "seconds on average")
+
 
 m.terminate()
 
